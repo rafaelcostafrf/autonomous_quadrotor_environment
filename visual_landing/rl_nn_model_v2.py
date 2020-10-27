5bdf4b4d617a7e281f9d52aea103916cde5d23b2
rafaelcostaf.rf@gmail.com <rafaelcostaf.rf@gmail.com>
2020-10-27 19:40:09 -0300

    Major Update , still not working, but several errors corrected.

--- visual_landing/rl_nn_model_v2.py
+++ visual_landing/rl_nn_model_v2.py
@@ -1,9 +1,12 @@
 import torch
+import cv2 as cv
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import MultivariateNormal
 from visual_landing.memory_leak import debug_gpu
+import torchvision.models as tvmodels
 import numpy as np
+import torchvision.models as models
 
 """
 MECHANICAL ENGINEERING POST-GRADUATE PROGRAM
@@ -17,35 +20,93 @@ DESCRIPTION:
     PPO neural network model
     hidden layers has 64 neurons
 """
-H0 = 64
-H1 = 48
-H2 = 48
+H0 = 512
+H1 = 512
+H11 = 512
+H2 = 512
 SENS_SIZE = 75
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
+    
+def plot_conv(x, name):
+    for channel in x[0]:
+        image = ((channel-channel.mean())/channel.std()).detach().cpu().numpy()
+    cv.imshow('conv_in_'+str(name), image)
+    cv.waitKey(1)
+    return
+
+def plot_conv3D(x, name):
+    for channel in x[0, 0]:
+        image = ((channel-channel.mean())/channel.std()).detach().cpu().numpy()
+    cv.imshow('conv_in_'+str(name), image)
+    cv.waitKey(1)
+    return
 
 class conv_forward(nn.Module):
     def __init__(self, T):
         super(conv_forward, self).__init__()
         
-        self.conv_1 = nn.Conv2d(in_channels = T, out_channels = 64, kernel_size=7, stride=2, padding = 0)       
-        self.conv_11 = nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size=7, stride=2, padding = 0) 
-        self.conv_2 = nn.Conv2d(in_channels = 128, out_channels = 256, kernel_size=3, stride=1, padding = 0)
-        self.conv_3 = nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size=3, stride=1, padding = 0)
-        self.fc_1 = nn.Linear(256*8**2, H0)
+        self.conv_1 = nn.Conv2d(in_channels = 3, out_channels = 16, kernel_size=7)       
+        self.conv_2 = nn.Conv2d(in_channels = 16, out_channels = 64, kernel_size=5)
+        self.conv_3 = nn.Conv2d(in_channels = 64, out_channels = 32, kernel_size=5)
+        # self.conv_4 = nn.Conv2d(in_channels = 128, out_channels = 32, kernel_size=3)
+        self.fc_1 = nn.Linear(32*24**2, H0)
         
     def forward(self, x):
+
+        x = torch.tanh(self.conv_1(x))
+        x = torch.max_pool2d(x, 2)
+        plot_conv(x, 1)
+
+        x = torch.tanh(self.conv_2(x))
+        x = torch.max_pool2d(x, 2)
+        plot_conv(x, 2)
+        
+
+        x = torch.tanh(self.conv_3(x))
+        x = torch.max_pool2d(x, 2)
+        plot_conv(x, 3)
+        
+        # x = torch.tanh(self.conv_4(x))
+        # x = torch.max_pool2d(x, 2)
+        # plot_conv(x, 4)
         # print(x.size())
-        x = F.max_pool2d(F.relu(self.conv_1(x)), 2)
-        # print(x.size())
-        x = F.relu(self.conv_11(x))
+        x = torch.tanh(self.fc_1(torch.flatten(x, start_dim=1)))
+
+        return x
+
+class conv3D_forward(nn.Module):
+    def __init__(self, T):
+        super(conv3D_forward, self).__init__()
+        
+        self.conv_1 = nn.Conv3d(in_channels = 1, out_channels = 32, kernel_size=(2,8,8), stride=(1,2,2))
+        self.conv_2 = nn.Conv3d(in_channels = 32, out_channels = 64, kernel_size=(2,4,4), stride=(1,2,2))
+        self.conv_3 = nn.Conv3d(in_channels = 64, out_channels = 64, kernel_size=(2,3,3), stride=(1,1,1))
+        # self.conv_4 = nn.Conv2d(in_channels = 128, out_channels = 32, kernel_size=3)
+        self.fc_1 = nn.Linear(64*1*17**2, H0)
+        
+    def forward(self, x):
+        x = torch.unsqueeze(x, dim=1)
+        x = F.relu(self.conv_1(x))
+        # x = torch.max_pool3d(x, (1,2,2))
+        # plot_conv3D(x, 1)
+
         x = F.relu(self.conv_2(x))
-        # print(x.size())
+        # x = torch.max_pool3d(x, (1,2,2))
+        # plot_conv(x, 2)
+        
+
         x = F.relu(self.conv_3(x))
+        # x = torch.max_pool3d(x, (1,2,2))
+        # plot_conv3D(x, 3)
+        
+        # x = torch.tanh(self.conv_4(x))
+        # x = torch.max_pool2d(x, 2)
+        # plot_conv(x, 4)
         # print(x.size())
-        x = F.relu(self.fc_1(torch.flatten(x, start_dim=1)))
-        # print(x.size())
+        x = torch.tanh(self.fc_1(torch.flatten(x, start_dim=1)))
+
         return x
 
 
@@ -53,44 +114,51 @@ class actor_nn(nn.Module):
     def __init__(self):
         super(actor_nn, self).__init__()
         
-        self.fc_1 = nn.Linear(H0, H1)
+        # self.fc_1 = nn.Linear(H0, H1)
         
-        self.fc_sens = nn.Linear(SENS_SIZE, H1)
+        self.fc_sens = nn.Linear(SENS_SIZE, H11)
         
-        self.fc_2 = nn.Linear(2*H1, H2)
-        self.fc_3 = nn.Linear(H2, 3)
+        self.fc_2 = nn.Linear(H1+H11, H2)
+        # self.fc_3 = nn.Linear(H2, H2)
+        self.fc_4 = nn.Linear(H2, 3)
         
     def forward(self, x, sens):
-        x = torch.tanh(self.fc_1(x))
 
+        # x = torch.tanh(self.fc_1(x))
+        
         sens_out = torch.tanh(self.fc_sens(sens))
 
         x = torch.cat((x, sens_out), dim=1)
 
         x = torch.tanh(self.fc_2(x))
-        x = torch.tanh(self.fc_3(x))
+        # x = torch.tanh(self.fc_3(x))
+        x = torch.tanh(self.fc_4(x))
         return x
     
 class critic_nn(nn.Module):
     def __init__(self):
         super(critic_nn, self).__init__()
         
-        self.fc_1 = nn.Linear(H0, H1)
+        # self.fc_1 = nn.Linear(H0, H1)
         
-        self.fc_sens = nn.Linear(SENS_SIZE, H1)
+        self.fc_sens = nn.Linear(SENS_SIZE+3, H11)
         
-        self.fc_2 = nn.Linear(2*H1, H2)
-        self.fc_3 = nn.Linear(H2, 1)
+        self.fc_2 = nn.Linear(H1+H11, H2)
+        # self.fc_3 = nn.Linear(H2, H2)
+        self.fc_4 = nn.Linear(H2, 1)
         
-    def forward(self, x, sens):
-        x = torch.tanh(self.fc_1(x))
-        
+    def forward(self, x, sens, action):
+
+        # x = torch.tanh(self.fc_1(x))
+
+        sens = torch.cat((sens, action), dim=1)
         sens_out = torch.tanh(self.fc_sens(sens))
 
         x = torch.cat((x, sens_out), dim=1)
 
         x = torch.tanh(self.fc_2(x))
-        x = self.fc_3(x)
+        # x = torch.tanh(self.fc_3(x))
+        x = self.fc_4(x)
         return x 
     
              
@@ -102,21 +170,31 @@ class ActorCritic(nn.Module):
         else:
             self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        self.conv_forward = conv_forward(T).to(self.device)
+        # self.resnet18 = models.resnet18().to(self.device)
+        self.conv = conv3D_forward(T).to(self.device)
         self.actor_nn = actor_nn().to(self.device)
         self.critic_nn = critic_nn().to(self.device)
         
         self.action_var = torch.full((action_dim,), action_std*action_std, dtype=torch.float).to(self.device)
         
-    def forward(self, image, sens):
-        conv_output = self.conv_forward(image)
+    def forward(self, image, sens, action):
+
+        # conv_output = self.conv(image)
+        # print(image[:, 0].size())
+        conv_output = self.conv(image)
+        # x_1 = self.conv(torch.unsqueeze(image[:, 1], 1)).detach()
+        # x_2 = self.conv(torch.unsqueeze(image[:, 2], 1)).detach()
+            
+        # conv_output = torch.cat([x_0, x_1, x_2], dim=1)
+
         actor_output = self.actor_nn(conv_output, sens)
-        critic_output = self.critic_nn(conv_output, sens)
+        critic_output = self.critic_nn(conv_output, sens, action)
+        
         return actor_output, critic_output
 
-    def act(self, state, sens, memory):
+    def act(self, state, sens, last_conv, memory):
 
-        action_mean, _ = self.forward(state, sens)
+        action_mean, _ = self.forward(state, sens, torch.zeros([1, 3]).to(self.device))
 
         cov_mat = torch.diag(self.action_var).to(self.device)
 
@@ -129,19 +207,21 @@ class ActorCritic(nn.Module):
             memory.actions = np.array(action.detach().cpu().numpy())
             memory.logprobs = np.array(action_logprob.detach().cpu().numpy()) 
             memory.sens = np.array(sens.detach().cpu().numpy())
+            memory.last_conv = np.array(last_conv.detach().cpu().numpy())
         else:
             memory.states = np.append(memory.states, state.detach().cpu().numpy(), axis=0)
             memory.actions = np.append(memory.actions, action.detach().cpu().numpy(), axis=0)
             memory.logprobs = np.append(memory.logprobs, action_logprob.detach().cpu().numpy(), axis=0)
             memory.sens = np.append(memory.sens, sens.detach().cpu().numpy(), axis=0)
+            memory.last_conv = np.append(memory.last_conv, last_conv.detach().cpu().numpy(), axis=0)
 
         action = action.detach().cpu().numpy().flatten()       
         
         return action
     
-    def evaluate(self, state, sens, action):   
+    def evaluate(self, state, sens, action, last_conv):   
         # print(state.size())
-        action_mean, state_value = self.forward(state, sens)
+        action_mean, state_value = self.forward(state, sens, action)
 
         action_var = self.action_var.expand_as(action_mean)
         cov_mat = torch.diag_embed(action_var).to(self.device)
@@ -150,5 +230,4 @@ class ActorCritic(nn.Module):
         
         action_logprobs = dist.log_prob(action)
         dist_entropy = dist.entropy()
-
         return action_logprobs, torch.squeeze(state_value), dist_entropy
--- visual_landing/rl_worker.py
+++ visual_landing/rl_worker.py
@@ -1,6 +1,3 @@
-import tracemalloc
-tracemalloc.start()
-
 
 # BASIC LIBRARIES
 import torch
@@ -26,17 +23,19 @@ from visual_landing.memory_leak import debug_gpu
 import matplotlib.pyplot as plt
 
 T = 5
-T_visual = 3
-EVAL_FREQUENCY = 2
+T_visual_time = [3, 2, 1, 0]
+T_visual = len(T_visual_time)
+T_total = T_visual_time[0]+1
+EVAL_FREQUENCY = 1
 
 TIME_STEP = 0.01
-TOTAL_STEPS = 3500
+TOTAL_STEPS = 2000
 
-IMAGE_LEN = np.array([128, 128])
-TASK_INTERVAL_STEPS = 10
-BATCH_SIZE = 250
-VELOCITY_SCALE = [1, 1, 2]
-
+IMAGE_LEN = np.array([88, 88])
+TASK_INTERVAL_STEPS = 20
+BATCH_SIZE = 256
+VELOCITY_SCALE = [0.2, 0.2, 0.25]
+VELOCITY_D = [0, 0, -0.25]
 #CONTROL POLICY
 AUX_DL = dl_in_gen(T, 13, 4)
 state_dim = AUX_DL.deep_learning_in_size
@@ -48,7 +47,9 @@ except:
     print('Could not load Control policy')
     sys.exit(1)  
 
-           
+PLOT_LENGTH = 100
+CONV_SIZE = 256
+       
 class quad_worker():
     def __init__(self, render, cv_cam, child_number = None, child = False):
         self.update = [0, None]
@@ -73,9 +74,8 @@ class quad_worker():
         
         #CONTROLLER POLICY
         self.quad_env = quad(TIME_STEP, TOTAL_STEPS, 1, T)
-        self.sensor = sensor(self.quad_env)
-        quad_random_pos = (np.random.random(3)-0.5)*2
-        states, action = self.quad_env.reset(np.array([quad_random_pos[0], 0, quad_random_pos[1], 0, quad_random_pos[2], 0, 1, 0, 0, 0, 0, 0, 0]))
+        self.sensor = sensor(self.quad_env)  
+        states, action = self.quad_reset_random()
         self.sensor.reset()
         self.aux_dl =  dl_in_gen(T, 13, 4)
         self.control_network_in = self.aux_dl.dl_input(states, action)
@@ -86,21 +86,18 @@ class quad_worker():
         self.wait_for_task = False
         self.visual_done = False
         self.ppo_calls = 0
-        self.render.taskMgr.setupTaskChain('async', numThreads = 4, tickClock = None,
+        self.render.taskMgr.setupTaskChain('async', numThreads = 16, tickClock = None,
                                    threadPriority = None, frameBudget = None,
                                    frameSync = None, timeslicePriority = None)
         # self.render.taskMgr.add(self.step, 'ppo step', taskChain = 'async')
         self.render.taskMgr.add(self.step, 'ppo step')
         #LDG TRAINING
-        self.vel_error = np.zeros(3)
+        self.vel_error = np.zeros([3])
         self.last_shaping = None
         
         #MARKER POSITION
-        random_marker_position = np.random.normal([0, 0], 0.8)
-        self.render.checker.setPos(*tuple(random_marker_position), 0.001)
-        self.marker_position = np.append(random_marker_position, 0.001)
         self.internal_frame = 0
-        
+        self.crtl_action = np.zeros(4)
         #CAMERA SETUP
         self.render.quad_model.setPos(0, 0, 0)
         self.render.quad_model.setHpr(0, 0, 0)
@@ -115,15 +112,29 @@ class quad_worker():
         
         #PLOT SETUP
         if not self.child:
-            self.time_step_plot = deque(maxlen=100)
-            self.reward_plot = deque(maxlen=100)
-            self.efforts_plot = deque(maxlen=100)
-            self.fig, self.axs = plt.subplots(2)
+            self.time_step_plot = deque(maxlen=PLOT_LENGTH)
+            self.reward_plot = deque(maxlen=PLOT_LENGTH)
+            self.efforts_plot = deque(maxlen=PLOT_LENGTH)
+            self.vel_plot = deque(maxlen=PLOT_LENGTH)
+            self.done_plot = deque(maxlen=PLOT_LENGTH)
+            self.fig, self.axs = plt.subplots(3)
             self.fig.suptitle('Event Viewer')
             plt.draw()
             plt.pause(1)
     
+        self.old_conv = torch.zeros([1, CONV_SIZE]).to(self.device)
         
+    def quad_reset_random(self):
+        random_marker_position = np.random.normal([0, 0], 0.8)
+        self.render.checker.setPos(*tuple(random_marker_position), 0.001)
+        self.marker_position = np.append(random_marker_position, 0.001)
+        
+        
+        quad_random_z = -5*np.random.random()+1
+        quad_random_xy = self.marker_position[0:2]+(np.random.random(2)-0.5)*quad_random_z/7*5/1.2
+        initial_state = np.array([quad_random_xy[0], 0, quad_random_xy[1], 0, quad_random_z, 0, 1, 0, 0, 0, 0, 0, 0])
+        states, action = self.quad_env.reset(initial_state)
+        return states, action
         
     def sensor_sp(self):
             _, self.velocity_accel, self.pos_accel = self.sensor.accel_int()
@@ -139,37 +150,44 @@ class quad_worker():
             return states_sens
             
     def image_zeros(self):
-        self.images = np.zeros([T_visual, IMAGE_LEN[0], IMAGE_LEN[0]])
+        self.images = np.zeros([T_visual_time[0]+1, IMAGE_LEN[0], IMAGE_LEN[0]])
      
     def image_roll(self, image):
-        self.images = np.roll(self.images, 1, axis=0)
+        self.print_image = image
+        self.images = np.roll(self.images, 1, 0)
         self.images[0, :, :] = image
+        # a = np.hstack((self.images[0], self.images[1], self.images[2]))
+        # cv.imshow('teste', a)
+        # cv.waitKey(1)
+        # for i, channel in enumerate(image):
+        #     try:
+        #         image[i] = (channel-np.mean(channel))/np.std(channel)
+        #     except:
+        #         image[i] = (channel-np.mean(channel))
         
         
     def take_picture(self):
         ret, image = self.cv_cam.get_image() 
         if ret:
-            return cv.cvtColor(image[:,:,0:4], cv.COLOR_BGR2GRAY)/255.0
+            return cv.cvtColor(image[:,:,0:3], cv.COLOR_BGR2GRAY)/255.0
+            # print(np.shape(image))
+            # return np.swapaxes(image[:,:,0:3]/255.0, 0, 2)
         else:
-            return np.zeros([IMAGE_LEN[0], IMAGE_LEN[0]])
+            return np.zeros([1, IMAGE_LEN[0], IMAGE_LEN[0]])
    
     def reset(self):
-        quad_random_pos = (np.random.random(3)-0.5)*2
-        states, action = self.quad_env.reset(np.array([quad_random_pos[0], 0, quad_random_pos[1], 0, quad_random_pos[2], 0, 1, 0, 0, 0, 0, 0, 0]))
+        states, action = self.quad_reset_random()
         self.sensor.reset()
         self.aux_dl =  dl_in_gen(T, 13, 4)
         self.control_network_in = self.aux_dl.dl_input(states, action)
         self.visual_done = False
         self.ppo_calls = 0
-        
+        self.old_conv = torch.zeros([1, CONV_SIZE]).to(self.device)
         #LDG TRAINING
-        self.vel_error = np.zeros(3)
+        self.vel_error = np.zeros([3])
         self.last_shaping = None
-        
+        self.crtl_action = np.zeros(4)
         #MARKER POSITION
-        random_marker_position = np.random.normal([0, 0], 0.8)
-        self.render.checker.setPos(*tuple(random_marker_position), 0.001)
-        self.marker_position = np.append(random_marker_position, 0.001)
         self.internal_frame = 0
         self.image_zeros()
         if self.train_time :
@@ -177,8 +195,10 @@ class quad_worker():
                 self.eval_flag = True
                 self.reward_accum = 0
             self.train_time = False  
+        
             
     def reset_policy(self):
+
         while True:
             f = open('./child_data/'+str(self.child_number)+'.txt', 'r')
             try:
@@ -188,7 +208,8 @@ class quad_worker():
             if a == 2:
                 break
             else:
-                time.sleep(0.1)            
+                time.sleep(0.1)    
+        self.reset()
         self.ldg_policy.policy.load_state_dict(torch.load('./PPO_landing.pth', map_location=self.device))
         self.ldg_policy.policy_old.load_state_dict(torch.load('./PPO_landing_old.pth', map_location=self.device))
                 
@@ -202,6 +223,7 @@ class quad_worker():
         torch.save(self.memory.rewards, child_name+'rewards.tch')  
         torch.save(self.memory.is_terminals, child_name+'is_terminals.tch')  
         torch.save(self.memory.sens, child_name+'sens.tch')
+        torch.save(self.memory.last_conv, child_name+'last_conv.tch')
         self.memory.clear_memory()
         f = open(child_name+'.txt','w')
         f.write(str(1))
@@ -233,26 +255,31 @@ class quad_worker():
                     is_terminals_temp = torch.load(child_name+'is_terminals.tch')
                     
                     sens_temp = torch.load(child_name+'sens.tch')
+                    
+                    last_conv_temp = torch.load(child_name+ 'last_conv.tch')
+                    
                     self.memory.actions = np.append(self.memory.actions, actions_temp, axis = 0)
                     self.memory.states = np.append(self.memory.states, states_temp, axis = 0)
                     self.memory.logprobs = np.append(self.memory.logprobs, logprobs_temp, axis = 0)
                     self.memory.rewards = np.append(self.memory.rewards, rewards_temp, axis = 0)
                     self.memory.is_terminals = np.append(self.memory.is_terminals, is_terminals_temp, axis = 0) 
                     self.memory.sens = np.append(self.memory.sens, sens_temp, axis = 0)
+                    self.memory.last_conv = np.append(self.memory.last_conv, last_conv_temp, axis=0)
                     del actions_temp
                     del states_temp
                     del logprobs_temp
                     del rewards_temp
                     del is_terminals_temp
                     del sens_temp
+                    del last_conv_temp
                     break
                 else:                            
                     time.sleep(0.1)
         self.n_samples += len(self.memory.rewards)
-        print('Total Number of Samples: {:d}'.format(self.n_samples), end='\t')           
+        print('\nTotal Number of Samples: {:d}'.format(self.n_samples), end='\t')           
         self.ldg_policy.update(self.memory)   
         self.memory.clear_memory()
-
+        self.reset()
         gc.collect()
 
         
@@ -267,7 +294,7 @@ class quad_worker():
     def render_position(self, coordinates, marker_position):
 
         self.render.checker.setPos(*tuple(marker_position))
-        
+
         pos = coordinates[0:3]
         ang = coordinates[3:6]
         w = coordinates[6::]
@@ -282,32 +309,62 @@ class quad_worker():
         self.render.dlightNP.setPos(*pos)
         for prop, a in zip(self.prop_models, self.a):
             prop.setHpr(a, 0, 0)
+        # self.render.graphicsEngine.renderFrame()
            
     def step(self, task):
-        for i in range(TASK_INTERVAL_STEPS):
+        if self.ppo_calls > T_total:  
+            image_in = np.array([self.images[T_visual_time[0]], self.images[T_visual_time[1]], self.images[T_visual_time[2]], self.images[T_visual_time[3]]])
 
+            image_plot = self.images[0]
+            
+            if not self.child:
+                cv.imshow('teste', image_plot)
+                cv.waitKey(1)
+            if self.eval_flag:                
+                network_in = torch.Tensor(image_in).to(self.device).detach()
+                control_in = torch.Tensor([self.control_network_in]).to(self.device).detach()
+                network_in = torch.unsqueeze(network_in, 0)
+                visual_action, _ = self.ldg_policy.policy_old(network_in, control_in, torch.zeros([1,3]).to(self.device))
+                
+                visual_action = visual_action.detach().cpu().numpy().flatten()
+
+                self.reward_accum += self.reward
+            else:
+                print('\rBatch Progress: {:.2%}'.format(len(self.memory.states)/BATCH_SIZE), end='          ')
+                if len(self.memory.states) == BATCH_SIZE:
+                    self.visual_done = True
+                    self.train_time = True
+                
+                network_in = image_in
+                control_in = torch.Tensor([self.control_network_in]).to(self.device).detach()
+                visual_action, self.old_conv = self.ldg_policy.select_action(network_in, control_in, self.old_conv, self.memory)
+              
+            self.vel_error = visual_action*VELOCITY_SCALE+VELOCITY_D
+            # print(self.vel_error)
+        internal_counter = 0
+        for i in range(TASK_INTERVAL_STEPS):
+            internal_counter+=1
             self.internal_frame += 1
             # LOWER CONTROL STEP  
+            states_sens = self.sensor_sp()
+            # CONTROL DIFFERENCE
+            error = np.array([[0, self.vel_error[0], 0, self.vel_error[1], 0, self.vel_error[2], 0, 0, 0, 0, 0, 0, 0, 0]])
+
+            self.control_network_in = self.aux_dl.dl_input(states_sens-error, [self.crtl_action])
             crtl_network_in = torch.FloatTensor(self.control_network_in).to('cpu')
 
-            crtl_action = CRTL_POLICY.actor(crtl_network_in).cpu().detach().numpy()
+            self.crtl_action = CRTL_POLICY.actor(crtl_network_in).cpu().detach().numpy()
 
-            states, _, _ = self.quad_env.step(crtl_action)
-            states_sens = self.sensor_sp()
-            
-            # CONTROL DIFFERENCE
-            error_smoothing = states_sens
-            error_smoothing[0, 1] -= self.vel_error[0] #- error_smoothing[0, 1]
-            error_smoothing[0, 3] -= self.vel_error[1] #- error_smoothing[0, 3]
-            error_smoothing[0, 5] -= self.vel_error[2] #- error_smoothing[0, 5]
-            
-            # print(error_smoothing, self.vel_error)
-            self.control_network_in = self.aux_dl.dl_input(error_smoothing, [crtl_action])
-                    
+            states, _, _ = self.quad_env.step(self.crtl_action)
+
             coordinates = np.concatenate((states[0, 0:5:2], self.quad_env.ang, np.zeros(4))) 
 
 
-        self.reward, self.last_shaping, self.visual_done = visual_reward(TOTAL_STEPS, self.marker_position, self.quad_env.state[0:5:2], self.quad_env.state[1:6:2], self.vel_error, self.last_shaping, self.internal_frame)
+        self.reward, self.last_shaping, self.visual_done = visual_reward(TOTAL_STEPS, self.marker_position, self.quad_env.state[0:5:2], self.quad_env.state[1:6:2], self.vel_error, self.last_shaping, self.internal_frame, self.quad_env.ang)
+
+        if self.ppo_calls > T_total and not self.eval_flag:
+            self.memory.rewards = np.append(self.memory.rewards, self.reward)            
+            self.memory.is_terminals = np.append(self.memory.is_terminals, self.visual_done)   
         
         self.render_position(coordinates, self.marker_position)
         # self.render.graphicsEngine.renderFrame()
@@ -325,41 +382,32 @@ class quad_worker():
         #         cv.waitKey(1)
                 
         
-        if self.ppo_calls >= T_visual:           
-            if self.eval_flag:
-                network_in = torch.Tensor(self.images).to(self.device).detach()
-                control_in = torch.Tensor([self.control_network_in]).to(self.device).detach()
-                network_in = torch.unsqueeze(network_in, 0)
-                visual_action, _ = self.ldg_policy.policy_old(network_in, control_in)
-                
-                visual_action = visual_action.detach().cpu().numpy().flatten()
-
-                self.reward_accum += self.reward
-            else:
-                print('\rBatch Progress: {:.2%}'.format(len(self.memory.states)/BATCH_SIZE), end='          ')
-                if len(self.memory.states) == BATCH_SIZE:
-                    self.visual_done = True
-                    self.train_time = True
-                
-                self.memory.rewards = np.append(self.memory.rewards, self.reward)
-            
-                self.memory.is_terminals = np.append(self.memory.is_terminals, self.visual_done)   
-                                
-                network_in = self.images
-                control_in = torch.Tensor([self.control_network_in]).to(self.device).detach()
-                visual_action = self.ldg_policy.select_action(network_in, control_in, self.memory)
-            
-            self.vel_error = visual_action*VELOCITY_SCALE    
-
+        
         if not self.child :                    
             self.reward_plot.append(self.reward)
             self.time_step_plot.append(self.ppo_calls)
-            self.efforts_plot.append(self.vel_error)        
+            self.efforts_plot.append(self.vel_error)  
+            self.vel_plot.append(self.quad_env.state[1:6:2])
+            self.done_plot.append(self.visual_done)
             if self.ppo_calls % 20 == 0:            
-                self.axs[0].clear()            
-                self.axs[1].clear()            
-                self.axs[0].plot(np.arange(len(self.efforts_plot)), self.efforts_plot)    
-                self.axs[1].plot(np.arange(len(self.efforts_plot)), self.reward_plot)
+                for axis in self.axs[0:2]:
+                    axis.clear()
+                    axis.grid()     
+                self.axs[0].plot(np.arange(len(self.efforts_plot)), np.array(self.efforts_plot)[:, 0], color='g', linewidth=0.5)   
+                self.axs[0].plot(np.arange(len(self.efforts_plot)), np.array(self.vel_plot)[:, 0], ls='--', color='g', linewidth=0.5)   
+                
+                self.axs[0].plot(np.arange(len(self.efforts_plot)), np.array(self.efforts_plot)[:, 1], color='r', linewidth=0.5)    
+                self.axs[0].plot(np.arange(len(self.efforts_plot)), np.array(self.vel_plot)[:, 1], ls='--', color='r', linewidth=0.5)   
+                
+                self.axs[0].plot(np.arange(len(self.efforts_plot)), np.array(self.efforts_plot)[:, 2], color='b', linewidth=0.5)    
+                self.axs[0].plot(np.arange(len(self.efforts_plot)), np.array(self.vel_plot)[:, 2], ls='--', color='b', linewidth=0.5)   
+                
+                self.axs[1].plot(np.arange(len(self.efforts_plot)), np.array(self.reward_plot), color='b', linewidth=0.2)  
+
+                for i, terminal in enumerate(self.done_plot):
+                    if terminal:
+                        self.axs[0].axvline(x=i, color='m', linewidth=1)
+                        self.axs[1].axvline(x=i, color='m', linewidth=1)
                 plt.draw()
                 self.fig.canvas.flush_events()
 
@@ -374,8 +422,11 @@ class quad_worker():
                 self.reset_policy()
             else:   
                 # print('Before Training Memory: {:.2f}Mb'.format(process.memory_info().rss/1000000), end='\t')
-                self.mother_train() 
+                self.mother_train()
+                self.axs[2].clear()
+                self.axs[2].grid()
                 self.train_calls += 1
+                self.axs[2].plot(np.arange(0,self.train_calls), self.ldg_policy.loss_memory)
                 # print('After Training Memory: {:.2f}Mb'.format(process.memory_info().rss/1000000))
 
                 
